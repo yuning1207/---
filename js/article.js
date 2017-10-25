eec897ebcb0bea354994dd6b5c51fa046b08da19
鱼宁 <1578517699@qq.com>
2017-10-25 22:04:56 +0800

    资源分享页面

--- js/article.js
+++ js/article.js
@@ -13,13 +13,18 @@ function addLoadEvent(func) {
 function article_right() {
     if (!document.getElementById)
         return false;
-
     var right_guide = document.getElementById("right_guide");
     var share_guide = document.getElementById("share_guide");
-    alert(right_guide);
     right_guide.onclick = function() {
-        share_guide.style.display = "block";
+        share_guide.style.right = "0"
         right_guide.style.display = "none";
+        document.onmousedown = function(ev) {
+            var oeven = ev || event;
+            if (oeven.button == 1 || oeven.button == 0 || oeven.button == 2) {
+                share_guide.style.right = "-28vw";
+                right_guide.style.display = "block";
+            }
+        }
     }
 }
 addLoadEvent(article_right);
